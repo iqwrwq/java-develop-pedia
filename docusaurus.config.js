8a2f7d370d1eff971dfa7a58d922cbb7e6cea9eb
Arthur <iqwrwq@celthin.speedport.ip>
2023-02-23 12:20:59 +0100

    FIRST

---
+++ docusaurus.config.js
@@ -0,0 +1,139 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+    title: 'Java-Develop-Pedia',
+    tagline: 'The place for Java Developers',
+    favicon: 'img/favicon.ico',
+
+    // Set the production url of your site here
+    url: 'https://your-docusaurus-test-site.com',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/',
+
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'iqwrwq', // Usually your GitHub org/user name.
+    projectName: 'java-develop-pedia', // Usually your repo name.
+
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+
+    // Even if you don't use internalization, you can use this field to set useful
+    // metadata like html lang. For example, if your site is Chinese, you may want
+    // to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
+    },
+
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        'https://github.com/iqwrwq/java-develop-pedia',
+                },
+                blog: {
+                    showReadingTime: true,
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        'https://github.com/iqwrwq/java-develop-pedia',
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            }),
+        ],
+    ],
+
+    themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            // Replace with your project's social card
+            image: 'img/docusaurus-social-card.jpg',
+            navbar: {
+                title: 'Java-Develop-Pedia',
+                logo: {
+                    alt: 'Java-Develop-Pedia',
+                    src: 'img/logo.svg',
+                },
+                items: [
+                    {
+                        type: 'doc',
+                        docId: 'intro',
+                        position: 'left',
+                        label: 'Roadmap',
+
+                    },
+                    {to: '/blog', label: 'Awesome', position: 'left'},
+                    {
+                        href: 'https://github.com/iqwrwq/java-develop-pedia',
+                        label: 'GitHub',
+                        position: 'right',
+                    },
+                ],
+            },
+            footer: {
+                style: 'dark',
+                links: [
+                    {
+                        title: 'Developer Wiki',
+                        items: [
+                            {
+                                label: 'Interactive Roadmap',
+                                to: '/docs/intro',
+                            },
+                            {
+                                label: 'Awesome List',
+                                to: '/docs/intro',
+                            },
+                            {
+                                label: 'Bucket List',
+                                to: '/docs/intro',
+                            },
+                        ],
+                    },
+                    {
+                        title: 'Source',
+                        items: [
+                            {
+                                label: 'java-developer-roadmap',
+                                href: 'https://github.com/s4kibs4mi/java-developer-roadmap',
+                            },
+                            {
+                                label: 'awesome-java',
+                                href: 'https://github.com/akullpp/awesome-java',
+                            },
+                        ],
+                    },
+                    {
+                        title: 'More from me',
+                        items: [
+                            {
+                                label: 'iqwrwq GitHub',
+                                href: 'https://github.com/iqwrwq',
+                            },
+                        ],
+                    },
+                ],
+            },
+            prism: {
+                darkTheme: darkCodeTheme,
+                theme: lightCodeTheme,
+            },
+        }),
+};
+
+module.exports = config;
---
+++ src/pages/my-react-page.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function myReactPage(){
+    return(
+            <h1>Hello World</h1>
+    )
+}
