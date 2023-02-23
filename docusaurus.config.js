000cd7c404297ce913cea09461d4e26af2c11261
iqwrwq <iqwrwq@celthin.speedport.ip>
2023-02-23 22:20:26 +0100

    style: adjust catching frase

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
     title: 'Java-Develop-Pedia',
-    tagline: 'The place for Java Developers',
+    tagline: 'Unlock your Java potential and code your way to success',
     favicon: 'img/favicon.ico',
 
     // Set the production url of your site here
